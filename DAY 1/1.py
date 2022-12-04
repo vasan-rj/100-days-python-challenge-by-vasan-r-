a92a64b6decf0a855540e768b0b5e565a59507de
vasan-rj <112774051+vasan-rj@users.noreply.github.com>
2022-12-04 13:39:43 +0530

    Add files via upload

---
+++ DAY 1/1.py
@@ -0,0 +1,16 @@
+#print statements in python 
+
+print("hello world!!") #print function   
+print("hi "+"how are you") #inside print everything is taken as string (string concatenation)
+print(" vasan "*3) #(string repetation)
+
+#python input statements 
+x=int(input("enter any integer number: "))#for getting user integer input
+print(x)
+y=float(input("enter any floating point value (ex:2.04,0,23,1.0....): "))
+print(y)
+z=input("enter anything ")#takes string input
+print(z)
+
+#f-read in python
+print(f'the values fetched from the user are {x},{y},{z}')
---
+++ DAY 1/2.py
@@ -0,0 +1,17 @@
+#python variables 
+#rules while defining a variable 
+#1) should not start with a underscore(_)
+#2) shold not start with a capital letter
+#3) shold not start with a number
+#4) shold not contain special character like !,@,#,$,%,^ etc....
+
+x=23
+print(x)
+y_=63
+print(y_)
+vasan_coder="python"
+print(vasan_coder)
+v1_coder='a'
+print(v1_coder)
+
+
